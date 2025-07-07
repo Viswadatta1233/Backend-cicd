ade9a47f13d57fb6125a2e79a9ed23e9e49bf511
Viswadatta1233 <n210059@rguktn.ac.in>
2025-07-07 18:09:20 +0530

    initialb

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello from Express + TypeScript API!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
